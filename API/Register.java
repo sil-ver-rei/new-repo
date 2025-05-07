2274352709be369c0c1cec2a624c57efbca07bf6
sil-ver-rei <skrudje593@gmail.com>
2025-05-07 20:35:44 +0300

    Для эмулятора

---
+++ API/Register.java
@@ -0,0 +1,11 @@
+package API;
+
+public class Register {
+    private String email;
+    private String password;
+
+    public Register(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
